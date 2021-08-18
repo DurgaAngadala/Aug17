41fa4edd19d49c2cf4163d51a4fa45a7e73fc2e3
DurgaAngadala <88447379+DurgaAngadala@users.noreply.github.com>
2021-08-18 17:40:24 +0530

    Update print array ele(pointers).c

--- print array ele(pointers).c
+++ print array ele(pointers).c
@@ -12,10 +12,7 @@ int main()
     int b[5]= {5,4,6,8,9};
     int *a=&b[0];
     int i;
-    for(i=0; i<5; i++)
-    printf("\n%d",*(a+i));
-    for(i=0; i<5; i++)
-    printf("\n %d at %u ",*(a+i),(a+i));
+    fun(a);
     return 0;
 }
 
@@ -25,8 +22,3 @@ OUTPUT:
 6
 8
 9
- 5 at 572107456 
- 4 at 572107460 
- 6 at 572107464 
- 8 at 572107468 
- 9 at 572107472 
