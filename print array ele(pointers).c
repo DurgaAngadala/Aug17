b31c80377e99935e48a9664505af7d8ca811b6cd
DurgaAngadala <88447379+DurgaAngadala@users.noreply.github.com>
2021-08-18 17:43:34 +0530

    Update print array ele(pointers).c

--- print array ele(pointers).c
+++ print array ele(pointers).c
@@ -22,3 +22,38 @@ OUTPUT:
 6
 8
 9
+    
+/*c program to print array elements using pointers*/    
+
+#include<stdio.h>
+#define N 5
+void fun(int *a)
+{ 
+    int i,b;
+    for(i=0; i<N; i++)
+    printf("\n%d",*(a+i));
+    printf("\nReverse order\n");
+    for(i = N-1; i>=0; i--)  
+    printf("%d\n", *(a+i));
+}
+int main()
+{   
+    int i;
+    int b[5]={5,4,6,8,9};
+    int *a=&b[0];
+    fun(a);
+    return 0;
+}
+
+OUTPUT:
+5
+4
+6
+8
+9
+Reverse order
+9
+8
+6
+4
+5
